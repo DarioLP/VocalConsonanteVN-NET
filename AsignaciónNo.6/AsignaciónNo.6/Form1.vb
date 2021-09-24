379aa05c92cc658dc759ae5e700675b8bd2991d7
DarioLP <lopezdario43@gmail.com>
2021-09-24 12:51:17 -0500

    LCase

--- AsignaciónNo.6/AsignaciónNo.6/Form1.vb
+++ AsignaciónNo.6/AsignaciónNo.6/Form1.vb
@@ -1,12 +1,19 @@
 ﻿Public Class Form1
-    Public inop1, cadena1, valCVocal1, voc As String
+    Public inop1, cadena1, valCVocal1, ValCcons, voc, con As String
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        inop1 = ""
         voc = ""
+
+
         inop1 = InputBox("Introducir texto")
 
         For x = 1 To inop1.Length
-            cadena1 = Mid(inop1, x, 1)
+
+            'LCase es está utilizando para detectar las letras tanto mayusculas como minúsculas
+            'Mid es una forma de espesificar la posicion de un caracter de una cadena
+            cadena1 = LCase(Mid(inop1, x, 1))
 
             If cadena1 = "a" Or cadena1 = "e" Or cadena1 = "i" Or cadena1 = "o" Or cadena1 = "u" Then
                 voc = voc + cadena1
@@ -14,28 +21,42 @@
 
         Next
 
-        MessageBox.Show("Letras de vocales: " + voc, "Opción 1", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        MessageBox.Show("letras de vocales: " + voc, "opción 1", MessageBoxButtons.OK, MessageBoxIcon.Information)
 
     End Sub
 
-    Dim CVocal As Integer
+    Dim CVocal, Ccons As Integer
 
 
 
     Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+
+        inop1 = ""
         CVocal = 0
+        Ccons = 0
+        voc = ""
+        con = ""
+
         inop1 = InputBox("Introducir texto")
 
         For x = 1 To inop1.Length
-            cadena1 = Mid(inop1, x, 1)
+
+            cadena1 = LCase(Mid(inop1, x, 1))
 
             If cadena1 = "a" Or cadena1 = "e" Or cadena1 = "i" Or cadena1 = "o" Or cadena1 = "u" Then
+
                 CVocal = CVocal + 1
+                voc = voc + cadena1
+
+            Else
+                Ccons = Ccons + 1
+                con = con + cadena1
             End If
 
         Next
-        valCvocal1 = Convert.ToString(CVocal)
-        MessageBox.Show("cantidad de vocales: " + valCVocal1, "Opción 1", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        valCVocal1 = Convert.ToString(CVocal)
+        valCcons = Convert.ToString(Ccons)
+        MessageBox.Show(inop1 + vbNewLine & "Cantidad de Vocales: " + valCVocal1 + " | " + voc + vbNewLine & "Cantidad de Consonantes: " + ValCcons + " | " + con, "Opción 1", MessageBoxButtons.OK, MessageBoxIcon.Information)
 
     End Sub
 
